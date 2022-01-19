05f9205e777340edc950c259279ce83995d178b2
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-01-19 12:34:41 +0900

    Update scratch.sh

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -3,3 +3,7 @@
 zcat data/impc/results/dataOverview.csv.gz | less
 zcat data/impc/results/fertility.csv.gz | less -N
 zcat data/impc/results/phenotypeHitsPerGene.csv.gz | less -N
+
+久野朗広助教の論文がPLoS Biologyに掲載されました。
+論文：https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.3001507
+プレスリリース：https://www.tsukuba.ac.jp/journal/medicine-health/20220119040000.html
