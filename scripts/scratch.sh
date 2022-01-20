1da8c4a9c01fb3b4389e7cb2596dfac287a739f8
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-01-20 20:08:52 +0900

    update

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-zcat data/impc/results/dataOverview.csv.gz | less
+ls -l -S data/impc/results/*.csv.gz
+find data/impc/results/*.csv.gz |
+    zcat data/impc/results/dataOverview.csv.gz | less
 zcat data/impc/results/fertility.csv.gz | less -N
 zcat data/impc/results/phenotypeHitsPerGene.csv.gz | less -N
 
-久野朗広助教の論文がPLoS Biologyに掲載されました。
-論文：https://journals.plos.org/plosbiology/article?id=10.1371/journal.pbio.3001507
-プレスリリース：https://www.tsukuba.ac.jp/journal/medicine-health/20220119040000.html
+# geneAndMPTermAssociationにはMGIのPhoenotype Overviewに関わるMPリストしか情報がない
+zcat data/impc/results/geneAndMPTermAssociation.csv.gz | less -N
+zcat data/impc/results/geneAndMPTermAssociation.csv.gz |
+    head |
+    awk -F, '{print $1, $2, $3}'
+zcat data/impc/results/geneAndMPTermAssociation.csv.gz |
+    grep -e "heterozygote" -e "hemizygote" -e "homozygote" |
+    cut -d, -f 1
+zcat data/impc/results/geneAndMPTermAssociation.csv.gz |
+    grep "Hnrnph2"
+
+# data/impc/results/statistical-results-ALL.csv.gz
+
+zcat data/impc/results/statistical-results-ALL.csv.gz |
+    head -n 1 >tmp.csv
+zcat data/impc/results/statistical-results-ALL.csv.gz |
+    grep Maf, >>tmp.csv
+
+gzip -c tmp.csv >tmp.csv.gz
+# END
