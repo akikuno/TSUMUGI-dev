5f5b426eb4f3273c42452adfd357cb4ba4eb5c93
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-02-20 14:13:22 +0900

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,19 +3,21 @@
 mkdir -p data/impc
 echo "*" >data/impc/.gitignore
 
-# rm -rf ftp*
-# wget - O - http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results |
-#     grep "^<a href=" |
-#     cut -d '"' -f 2 |
-#     while read -r line; do
-#         wget -P data/impc http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results/"$line"
-#     done
+mkdir -p data/impc/results/
+rm -rf ftp*
+wget -O - http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results |
+    grep "^<a href=" |
+    cut -d '"' -f 2 |
+    while read -r line; do
+        echo "$line"
+        wget -P data/impc/results/ http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results/"$line"
+    done
 
 ###############################################################################
 # Download  IMPC Release: 15.1
 ###############################################################################
 
-time wget -c -r -np -l 0 ftp://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/
+# time wget -c -r -np -l 0 ftp://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/
 
 # move to "data/impc" manually
 
@@ -23,25 +25,25 @@ time wget -c -r -np -l 0 ftp://ftp.ebi.ac.uk/pub/databases/impc/all-data-release
 # MD Check Sum
 ###############################################################################
 
-(
-    cd data/impc
-    find cores/*.md5 |
-        while read -r file; do
-            md5sum -c "$file" ||
-                echo "$file is not propary downloaded"
-        done
-)
+# (
+#     cd data/impc
+#     find cores/*.md5 |
+#         while read -r file; do
+#             md5sum -c "$file" ||
+#                 echo "$file is not propary downloaded"
+#         done
+# )
 
 ###############################################################################
 # Open TAR files
 ###############################################################################
 
-ls -l data/impc/cores/*tar -S
+# ls -l data/impc/cores/*tar -S
 
-find data/impc/cores/*tar |
-    while read -r tar; do
-        echo $tar
-        tar -xf "$tar" -C "$(dirname $tar)"
-    done
+# find data/impc/cores/*tar |
+#     while read -r tar; do
+#         echo $tar
+#         tar -xf "$tar" -C "$(dirname $tar)"
+#     done
 
 # END
