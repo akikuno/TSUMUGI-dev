8c02f74ed5a3284485edc06b5433c31502b8ab53
Akihiro Kuno <akikuno@users.noreply.github.com>
2022-03-03 19:30:19 +0900

    update

---
+++ scripts/compute_module.R
@@ -0,0 +1,41 @@
+###########################################################
+# Initialization
+###########################################################
+
+options(repos = "http://cran.us.r-project.org")
+if (!require("pacman", q = TRUE)) install.packages("pacman")
+pacman::p_load(tidyverse, igraph, bipartite)
+
+###########################################################
+# Import data
+###########################################################
+
+df <- read_csv("reports/symbol_mp_zygosity_effectsize.csv")
+
+###########################################################
+# Compute Modules
+###########################################################
+
+df$effect_size <- abs(df$effect_size)
+
+g <- graph_from_data_frame(df)
+
+start <- Sys.time() #---------
+modules <- as_adjacency_matrix(g, attr = "effect_size") %>%
+    as.matrix() %>%
+    computeModules()
+end <- Sys.time() #---------
+
+end - start # 44 min
+
+list_modules <- listModuleInformation(modules)
+
+str(list_modules)
+###########################################################
+# Export
+###########################################################
+save(list_modules, file = "reports/list_modules.rda")
+
+write_csv(df_mp_term, "reports/symbol_mp_zygosity_effectsize.csv")
+
+# End #####################################################
