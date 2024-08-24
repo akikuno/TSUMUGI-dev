5d6e7c5a2e4ee34ab93f368585dac66081fa2338
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-08-24 15:59:56 +0900

    Add font scale bar

--- web/network/js/male_infertility.js
+++ web/network/js/male_infertility.js
@@ -2,14 +2,6 @@
 // Input handling
 // ========================================================
 
-// const elements = [
-//     { data: { id: 'a', label: 'Nanog', annotation: ['hoge', 'hooo'], node_color: 3 } },
-//     { data: { id: 'b', label: 'Pou5f1', annotation: 'fuga', node_color: 10 } },
-//     { data: { id: 'c', label: 'Sox2', annotation: 'foo', node_color: 1 } },
-//     { data: { source: 'a', target: 'b', annotation: 'Foo', edge_size: 0.5 } },
-//     { data: { source: 'a', target: 'c', annotation: 'FooBar', edge_size: 1.5 } },
-// ];
-
 const elements = (function () {
     const req = new XMLHttpRequest();
     let result = null;
@@ -18,8 +10,7 @@ const elements = (function () {
             result = JSON.parse(req.responseText);
         }
     };
-    req.open("GET", "https://www.md.tsukuba.ac.jp/LabAnimalResCNT/test-tsumugi/network/data/male_infertility.json", false);
-    // req.open("GET", "https://gist.githubusercontent.com/akikuno/831ec21615501cc7bd1d381c5e56ebd2/raw/3615e66d75627351f3b3c2300cc27101d46cd749/network.json", false);
+    req.open("GET", "https://gist.githubusercontent.com/akikuno/831ec21615501cc7bd1d381c5e56ebd2/raw/3615e66d75627351f3b3c2300cc27101d46cd749/network.json", false);
     req.send(null);
     return result;
 })();
@@ -36,25 +27,26 @@ const nodeMax = Math.max(...nodeSizes);
 const edgeMin = Math.min(...edgeSizes);
 const edgeMax = Math.max(...edgeSizes);
 
+// Function to normalize edge sizes to a range of 0.5 to 2
+function normalizeEdgeSize(edgeSize) {
+    const minRange = 1;
+    const maxRange = 10;
+    return minRange + (edgeSize - edgeMin) * (maxRange - minRange) / (edgeMax - edgeMin);
+}
+
 function scaleToOriginalRange(value, minValue, maxValue) {
-    // Scale the slider value (1-10) to the original range
     return minValue + (value - 1) * (maxValue - minValue) / 9;
 }
 
 function scaleToSliderRange(value, minValue, maxValue) {
-    // Scale the original range value to the slider value (1-10)
     return 1 + (value - minValue) * 9 / (maxValue - minValue);
 }
 
 function getColorForValue(value) {
-    // Assumes value is in the range of 1 to 10
     const ratio = (value - 1) / (10 - 1);
-
-    // Gradient from light yellow to orange
     const r1 = 248, g1 = 229, b1 = 140; // Light Yellow
     const r2 = 255, g2 = 140, b2 = 0;   // Orange
 
-    // Linear interpolation
     const r = Math.round(r1 + (r2 - r1) * ratio);
     const g = Math.round(g1 + (g2 - g1) * ratio);
     const b = Math.round(b1 + (b2 - b1) * ratio);
@@ -62,57 +54,11 @@ function getColorForValue(value) {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-function filterElements() {
-    const nodeColorSliderValue = parseFloat(document.getElementById('node-color-slider').value);
-    const edgeSizeSliderValue = parseFloat(document.getElementById('edge-size-slider').value);
-
-    const nodeThreshold = scaleToOriginalRange(nodeColorSliderValue, nodeMin, nodeMax);
-    const edgeThreshold = scaleToOriginalRange(edgeSizeSliderValue, edgeMin, edgeMax);
-
-    // Filter nodes
-    cy.nodes().forEach(function (node) {
-        const nodeColor = node.data('node_color');
-        node.style('display', nodeColor >= nodeThreshold ? 'element' : 'none');
-    });
-
-    // Filter edges
-    cy.edges().forEach(function (edge) {
-        const edgeSize = edge.data('edge_size');
-        const sourceNode = cy.getElementById(edge.data('source'));
-        const targetNode = cy.getElementById(edge.data('target'));
-
-        // Edge filtering conditions
-        if (sourceNode.style('display') === 'element' && targetNode.style('display') === 'element' && edgeSize >= edgeThreshold) {
-            edge.style('display', 'element');
-        } else {
-            edge.style('display', 'none');
-        }
-    });
-
-    // Hide nodes with degree 0 after filtering edges
-    cy.nodes().forEach(function (node) {
-        const connectedEdges = node.connectedEdges().filter(edge => edge.style('display') === 'element');
-        if (connectedEdges.length === 0) {
-            node.style('display', 'none');
-        }
-    });
-
-    // Re-run the layout
-    cy.layout({ name: 'cose', componentSpacing: 100, nodeRepulsion: 100000 }).run();  // Re-run the currently used layout
-}
-
-// Initial slider setup
-document.getElementById('node-color-slider').value = 1;
-document.getElementById('node-color-value').textContent = 1;
-
-document.getElementById('edge-size-slider').value = 1;
-document.getElementById('edge-size-value').textContent = 1;
-
-
 // ========================================================
 // Cytoscape handling
 // ========================================================
 
+let currentLayout = 'cose';
 
 const cy = cytoscape({
     container: document.querySelector('.cy'),
@@ -125,7 +71,7 @@ const cy = cytoscape({
                 'text-valign': 'center',
                 'text-halign': 'center',
                 'font-family': 'Roboto',
-                'font-size': "20px",
+                'font-size': '20px',
                 'width': 15,
                 'height': 15,
                 'background-color': function (ele) {
@@ -139,18 +85,20 @@ const cy = cytoscape({
             style: {
                 'curve-style': 'bezier',
                 'text-rotation': 'autorotate',
-                'width': 'data(edge_size)',
+                'width': function (ele) {
+                    const originalSize = ele.data('edge_size');
+                    return normalizeEdgeSize(originalSize);
+                }
             }
         }
     ],
-    layout: { name: 'cose', }
+    layout: { name: currentLayout }
 });
-
 
 // Event listener for layout change
 document.getElementById('layout-dropdown').addEventListener('change', function () {
-    const layout = this.value;
-    cy.layout({ name: layout }).run();
+    currentLayout = this.value;
+    cy.layout({ name: currentLayout }).run();
 });
 
 // Event listeners for sliders
@@ -164,15 +112,58 @@ document.getElementById('edge-size-slider').addEventListener('input', function (
     filterElements();
 });
 
-// Initial filtering and node color setup
-filterElements();
+document.getElementById('font-size-slider').addEventListener('input', function () {
+    const fontSize = this.value + 'px';
+    document.getElementById('font-size-value').textContent = this.value;
+    cy.style()
+        .selector('node')
+        .style('font-size', fontSize)
+        .update();
+});
 
+// ========================================================
+// Filtering function
+// ========================================================
+
+function filterElements() {
+    const nodeColorSliderValue = parseFloat(document.getElementById('node-color-slider').value);
+    const edgeSizeSliderValue = parseFloat(document.getElementById('edge-size-slider').value);
+
+    const nodeThreshold = scaleToOriginalRange(nodeColorSliderValue, nodeMin, nodeMax);
+    const edgeThreshold = scaleToOriginalRange(edgeSizeSliderValue, edgeMin, edgeMax);
+
+    cy.nodes().forEach(function (node) {
+        const nodeColor = node.data('node_color');
+        node.style('display', nodeColor >= nodeThreshold ? 'element' : 'none');
+    });
+
+    cy.edges().forEach(function (edge) {
+        const edgeSize = edge.data('edge_size');
+        const sourceNode = cy.getElementById(edge.data('source'));
+        const targetNode = cy.getElementById(edge.data('target'));
+
+        if (sourceNode.style('display') === 'element' && targetNode.style('display') === 'element' && edgeSize >= edgeThreshold) {
+            edge.style('display', 'element');
+        } else {
+            edge.style('display', 'none');
+        }
+    });
+
+    cy.nodes().forEach(function (node) {
+        const connectedEdges = node.connectedEdges().filter(edge => edge.style('display') === 'element');
+        if (connectedEdges.length === 0) {
+            node.style('display', 'none');
+        }
+    });
+
+    cy.layout({ name: currentLayout, componentSpacing: 100, nodeRepulsion: 100 }).run();
+}
 
 // ========================================================
 // Tooltip handling
 // ========================================================
 
-cy.on('mouseover', 'node, edge', function (event) {
+cy.on('tap', 'node, edge', function (event) {
     const data = event.target.data();
     let tooltipText = '';
 
