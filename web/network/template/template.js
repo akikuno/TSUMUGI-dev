95d384de157610000902bd9747377150dfee822d
akikuno <akikuno@users.noreply.github.com>
2024-09-19 09:42:20 +0900

    Hide tooltip when tapping on background

--- web/network/template/template.js
+++ web/network/template/template.js
@@ -2,17 +2,6 @@
 // Input handling
 // ========================================================
 
-// const elements = [
-//     { data: { id: 'Nanog', label: 'Nanog', annotation: ['hoge', 'hooo'], node_color: 50, } },
-//     { data: { id: 'Pou5f1', label: 'Pou5f1', annotation: 'fuga', node_color: 100, } },
-//     { data: { id: 'Sox2', label: 'Sox2', annotation: 'foo', node_color: 3, } },
-//     { data: { source: 'Nanog', target: 'Pou5f1', annotation: ['Foo', 'FooBar'], edge_size: 5 } },
-//     { data: { source: 'Nanog', target: 'Sox2', annotation: 'FooBar', edge_size: 1 } },
-//     { data: { source: 'Sox2', target: 'Pou5f1', annotation: 'FooBar', edge_size: 10 } },
-// ];
-
-// const map_symbol_to_id = { 'Nanog': 'MGI:97281', 'Pou5f1': 'MGI:1352748', 'Sox2': 'MGI:96217' };
-
 const elements = (function () {
     const req = new XMLHttpRequest();
     let result = null;
@@ -378,6 +367,18 @@ cy.on('tap', 'node, edge', function (event) {
         tooltip.style.cursor = 'move';
     });
 });
+
+
+// Hide tooltip when tapping on background
+cy.on('tap', function (event) {
+    // If the clicked element is not a node or edge, remove the tooltip
+    if (event.target === cy) {
+        document.querySelectorAll('.cy-tooltip').forEach(function (el) {
+            el.remove();
+        });
+    }
+});
+
 
 // ========================================================
 // Exporter
