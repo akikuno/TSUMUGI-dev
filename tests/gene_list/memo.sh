63451f0e8b106c15e4bc1ed4ecc38ba82a47a0de
akikuno <akikuno@users.noreply.github.com>
2025-03-02 06:28:30 +0900

    Add Ddx46

--- tests/gene_list/memo.sh
+++ tests/gene_list/memo.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
 
-```bash
+zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | jq -c '.[]' | grep "Rab10" | grep "Asxl1" | jq -s '.'
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | grep -n "Rab10"
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | grep -n "Asxl1"
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | head -n 50 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | head -n 450 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | head -n 800 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Asxl1.json.gz | head -n 950 | tail -n 50
-```
 
-```bash
+zcat test-tsumugi/data/genesymbol/Rab10.json.gz | jq -c '.[]' | grep "Rab10" | grep "Asxl1"
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | grep -n "Rab10"
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | grep -n "Asxl1"
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | head -n 50 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | head -n 450 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | head -n 850 | tail -n 50
 zcat test-tsumugi/data/genesymbol/Rab10.json.gz | head -n 950 | tail -n 50
-```
+
+zcat test-tsumugi/data/genesymbol/Ddx46.json.gz | grep -n "Ddx46"
+zcat test-tsumugi/data/genesymbol/Ddx46.json.gz | head -n 50 | tail -n 50
