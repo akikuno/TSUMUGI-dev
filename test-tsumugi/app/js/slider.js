8804f851a7d0201705805bc9738af50e680dee04
akikuno <akikuno@users.noreply.github.com>
2025-03-03 11:12:18 +0900

    Add `export`

--- test-tsumugi/app/js/slider.js
+++ test-tsumugi/app/js/slider.js
@@ -1,5 +1,5 @@
 // 汎用スライダー作成関数（単一値 & 範囲スライダーに対応）
-function createSlider(id, start, min, max, step, updateCallback, isRange = false) {
+export function createSlider(id, start, min, max, step, updateCallback, isRange = false) {
     const slider = document.getElementById(id);
     if (!slider) {
         console.error(`Slider with ID '${id}' not found.`);
