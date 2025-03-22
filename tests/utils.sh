31712b028108f1b1daf696f873f95c447deb118e
akikuno <akikuno@users.noreply.github.com>
2025-03-23 07:46:48 +0900

    Phenotypes for which no phenotype-similar gene groups were detected have been removed from `available_mp_terms.json` to prevent them from being selected. Issue: [#58]

--- tests/utils.sh
+++ tests/utils.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-WORD="phenotypeForm"
+WORD="available_gene_symbols"
 
-find TSUMUGI/template -type f |
-    xargs grep "$WORD"
+find notebooks/ -type f | xargs grep "$WORD"
+find TSUMUGI/template -type f | xargs grep "$WORD"
 
 type prettier || conda install -y -n env-tsumugi conda-forge::prettier
 prettier --write "TSUMUGI/template/**/*" --print-width 120 --prose-wrap never --tab-width 4
