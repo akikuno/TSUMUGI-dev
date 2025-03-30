ad11881f9afa64ef3164e8a6a43e8b6adba79430
akikuno <akikuno@users.noreply.github.com>
2025-03-30 13:34:57 +0900

    HTMLテンプレートの共通化 #68

--- tests/utils.sh
+++ tests/utils.sh
@@ -10,5 +10,7 @@ conda activate env-tsumugi
 
 type prettier || conda install -y -n env-tsumugi conda-forge::prettier
 prettier --write "TSUMUGI/template/**/*" --print-width 120 --prose-wrap never --tab-width 4
-
 prettier --write "test-tsumugi/**/*" --print-width 120 --prose-wrap never --tab-width 4
+
+
+prettier --write "TSUMUGI/app/**/*" --print-width 120 --prose-wrap never --tab-width 4
