1791cac0cb783b4f52e1782356a4f6deb2334afe
akikuno <akikuno@users.noreply.github.com>
2025-10-14 12:40:29 +0900

    Remove unused directory

--- TSUMUGI/template/template-app-js/node_color_initialization.js
+++
@@ -1,3 +0,0 @@
-// Initialization of the Node color slider
-const nodeSlider = document.getElementById("filter-node-slider");
-noUiSlider.create(nodeSlider, { start: [1, 10], connect: true, range: { min: 1, max: 10 }, step: 1 });
--- TSUMUGI/template/template-app-js/node_color_update.js
+++
@@ -1,8 +0,0 @@
-// Update the slider values when the sliders are moved
-nodeSlider.noUiSlider.on("update", function (values) {
-    const intValues = values.map((value) => Math.round(value));
-    document.getElementById("node-color-value").textContent = intValues.join(" - ");
-    filterByNodeColorAndEdgeSize();
-});
-
-
